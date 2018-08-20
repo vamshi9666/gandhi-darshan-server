4cc028f8749eb40bdb900ea6f6a787b56dfa2ead
vamshi9666 <vamshi123nimmakuri123@gmail.com>
2018-08-20 11:05:06 +0530

    done with patching in best practice

--- routes/event.js
+++ routes/event.js
@@ -5,4 +5,8 @@ const eventController = require('../controllers/events')
 
 router.get('/',eventController.get_all_events)
 
+router.post('/',eventController.add_event)
+
+router.patch('/:id',eventController.update_event)
+
 module.exports = router
